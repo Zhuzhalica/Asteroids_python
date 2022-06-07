f1374417eda6108651dbb2e87bd10d43c6d8c2b1
MarkovGerman <German.Markov@urfu.me>
2022-06-07 17:25:44 +0500

    UFO

--- space_ship.py
+++ space_ship.py
@@ -1,12 +1,13 @@
 import pygame
 
 
-class SpaceShip:
+class SpaceShip(pygame.sprite.Sprite):
     def __init__(self, screen):
         """создаем корабль"""
-
+        pygame.sprite.Sprite.__init__(self)
+        self.health = 50
         self.screen = screen
-        self.image = pygame.image.load("image/pixil-frame-0.png")
+        self.image = pygame.transform.scale(pygame.image.load("image/pixil-frame-0.png"),(50,50))
 
         # стартовая позиция
         self.screen_rect = screen.get_rect()
@@ -32,17 +33,19 @@ class SpaceShip:
 
     def update_ship(self):
         """обновление позиции корабля"""
+        if self.health <= 0:
+            self.kill()
         if self.move_up and self.InScreen():
-            self.speed = min(self.speed + 0.05, self.max_speed)
+            self.speed = min(self.speed + 0.4, self.max_speed)
             self.position += self.direction * self.speed
         elif not self.move_up and self.InScreen():
-            self.speed = max(self.speed - 0.05, 0)
+            self.speed = max(self.speed - 0.4, 0)
             self.position += self.direction * self.speed
 
         if self.turn_right:
-            self.direction.rotate_ip(0.1)
+            self.direction.rotate_ip(0.8)
         if self.turn_left:
-            self.direction.rotate_ip(-0.1)
+            self.direction.rotate_ip(-0.8)
 
         angle = self.direction.angle_to((1, 0))
         rotated_image = pygame.transform.rotate(self.image, angle - 90)
@@ -55,3 +58,7 @@ class SpaceShip:
                self.rect.bottom < self.screen_rect.bottom and \
                self.rect.left > self.screen_rect.left and \
                self.rect.right < self.screen_rect.right
+
+
+    def TakeDamage(self, damage):
+        self.health -= damage
